a743d1e4708ed0c2cf8e01833397d9f3af887978
Christopher Leal <christopher.leal@globant.com>
2022-01-11 23:36:39 -0600

    jsdoc added

--- src/utils/hash.js
+++ src/utils/hash.js
@@ -1,6 +1,13 @@
 import crypto from 'crypto'
 import { getPrevHash } from './logFile'
 
+/**
+ * Calculate hash from a message, previous hash and nonce
+ * @param {string} message Message to hash
+ * @param {string} prevHash Hash from the previous hash
+ * @param {number} nonce Proof of Word
+ * @returns {string} a hashed string
+ */
 const calculateHash = (message, prevHash = '', nonce) =>
   crypto
     .createHash('sha256')
@@ -8,6 +15,11 @@ const calculateHash = (message, prevHash = '', nonce) =>
     .digest('hex')
     .toString()
 
+/**
+ * Generate hash from a message
+ * @param {string} message Message to hash
+ * @returns {object} return the hash and the nonce
+ */
 const generateHash = (message) => {
   let nonce = 0
   const prevHash = getPrevHash()
@@ -19,6 +31,12 @@ const generateHash = (message) => {
   return { hash, nonce }
 }
 
+/**
+ *
+ * @param {string} hash Hash to check
+ * @param {number} difficulty Number of zeros at the begining
+ * @returns {boolean} a boolean true if the hash matches with the regex
+ */
 const zerosCheck = (hash, difficulty = 2) => {
   const zerosAmount = Array(difficulty + 1).join('0')
   const pattern = new RegExp(`^${zerosAmount}.*`)
