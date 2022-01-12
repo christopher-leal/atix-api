e4bfbef990baf7514886b26a105911c12dc811f8
Christopher Leal <christopher.leal@globant.com>
2022-01-11 23:37:32 -0600

    testing root route

---
+++ src/app.test.js
@@ -0,0 +1,19 @@
+import request from 'supertest'
+import app from './app'
+
+describe('Testing main route', () => {
+  it('should return a status code 200 and a json correct', (done) => {
+    request(app)
+      .get('/')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err)
+        expect(typeof res.body).toBe('object')
+        expect(res.body.success).toBe(true)
+        expect(res.body.message).toBe('Server is online')
+
+        return done()
+      })
+  })
+})
