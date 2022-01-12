1b930dbd47e8b93312a5146b70996f3c7b83bf4e
Christopher Leal <christopher.leal@globant.com>
2022-01-11 23:42:18 -0600

    messages controller done

--- src/controllers/messages.js
+++ src/controllers/messages.js
@@ -1,6 +1,15 @@
+import { saveData } from './../utils/logFile'
+import { generateHash } from './../utils/hash'
+import logger from './../utils/logger'
+
 const saveMessage = async (req, res) => {
-  return res.json({
-    ok: true
+  const { message } = req.body
+  const hashData = generateHash(message)
+  logger.info('Hash generated successfully')
+  const dataToSave = `${hashData.hash},${message},${hashData.nonce}`
+  saveData(`${dataToSave} \r\n`)
+  return res.status(200).json({
+    success: true
   })
 }
 
