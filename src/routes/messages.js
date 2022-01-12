67e68d05f64019c5639b29af3c5b6d09b3968e17
Christopher Leal <christopher.leal@globant.com>
2022-01-11 19:05:35 -0600

    middleware required on post route

--- src/routes/messages.js
+++ src/routes/messages.js
@@ -1,9 +1,14 @@
 import { Router } from 'express'
 import messagesController from './../controllers/messages'
+import { body } from 'express-validator'
+import validateFields from './../middleware/validateFields'
 
 const router = Router()
 
 router
-  .post('/', messagesController.saveMessage)
+  .post('/', [
+    body('message').notEmpty().withMessage('Message is required'),
+    validateFields
+  ], messagesController.saveMessage)
 
 export default router
